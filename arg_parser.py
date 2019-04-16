8a17d3c9ca1ffcfd51745d8336978a1858826529
Adamantios <manoszaras94@gmail.com>
2019-04-16 14:42:12 +0300

    Show default values.

--- arg_parser.py
+++ arg_parser.py
@@ -1,5 +1,14 @@
 from argparse import ArgumentParser
 
+# Default arguments.
+ITERATIONS = 1000
+CREATE_GIF = False
+FRAMES = 10
+DURATION = 1200
+CONTENT_WEIGHT = 1.2
+STYLE_WEIGHT = 1
+TV_WEIGHT = 1
+
 
 def create_parser() -> ArgumentParser:
     """
@@ -16,20 +25,21 @@ def create_parser() -> ArgumentParser:
                         help='Path to the style image.')
     parser.add_argument('combined_filename', metavar='result', type=str,
                         help='Prefix for the saved image results.')
-    parser.add_argument('-i', '--iter', type=int, default=1000, required=False,
-                        help='Number of iterations for the optimizer.\n'
+    parser.add_argument('-i', '--iter', type=int, default=ITERATIONS, required=False,
+                        help='Number of iterations for the optimizer (default %(default)s).\n'
                              'If --gif is passed, this is the number of iterations for each frame.')
-    parser.add_argument('-g', '--gif', default=False, required=False, action='store_true',
+    parser.add_argument('-g', '--gif', default=CREATE_GIF, required=False, action='store_true',
                         help='Whether a gif of the procedure should be created.')
-    parser.add_argument('-f', '--frames', type=int, default=10, required=False,
-                        help='Number of the gif frames.\nIgnored if --gif is not passed.')
-    parser.add_argument('-d', '--duration', type=int, default=1200, required=False,
-                        help='Duration of the gif in milliseconds.\nIgnored if --gif is not passed.')
-    parser.add_argument('-cw', '--content_weight', type=float, default=1.2, required=False,
-                        help='Content weight.')
-    parser.add_argument('-sw', '--style_weight', type=float, default=1.0, required=False,
-                        help='Style weight.')
-    parser.add_argument('-tvw', '--tv_weight', type=float, default=1.0, required=False,
-                        help='Total Variation weight.')
+    parser.add_argument('-f', '--frames', type=int, default=FRAMES, required=False,
+                        help='Number of the gif frames (default %(default)s).\nIgnored if --gif is not passed.')
+    parser.add_argument('-d', '--duration', type=int, default=DURATION, required=False,
+                        help='Duration of the gif in milliseconds (default %(default)s).\n'
+                             'Ignored if --gif is not passed.')
+    parser.add_argument('-cw', '--content_weight', type=float, default=CONTENT_WEIGHT, required=False,
+                        help='Content weight (default %(default)s).')
+    parser.add_argument('-sw', '--style_weight', type=float, default=STYLE_WEIGHT, required=False,
+                        help='Style weight (default %(default)s).')
+    parser.add_argument('-tvw', '--tv_weight', type=float, default=TV_WEIGHT, required=False,
+                        help='Total Variation weight (default %(default)s).')
 
     return parser
