742b99692379ff6da81d6eba486a377c7474cdfb
Adamantios <manoszaras94@gmail.com>
2019-04-25 14:07:30 +0300

    Make main more readable. Create checkpoint if the user wants. Create the model with starting weights, if they exist.

--- train_network.py
+++ train_network.py
@@ -1,6 +1,9 @@
+import os
 import pickle
 from typing import Union
 
+from tensorflow.python.keras import Sequential
+from tensorflow.python.keras.callbacks import ModelCheckpoint
 from tensorflow.python.keras.datasets import cifar10
 from tensorflow.python.keras.optimizers import rmsprop, adam, adamax, adadelta, adagrad, sgd
 from tensorflow.python.keras.utils import to_categorical
@@ -20,6 +23,16 @@ def preprocess_images(images_array):
         images_array[i, :, :, :] = StyleTransferCustom.preprocess_image(image.astype('float32'))
 
     return images_array
+
+
+def create_model() -> Sequential:
+    if start_point != '':
+        if os.path.isfile(start_point):
+            return StyleTransferCustom.network(input_shape=x_train.shape[1:], weights_path=start_point)
+        else:
+            raise FileNotFoundError('Checkpoint file \'{}\' not found.'.format(start_point))
+    else:
+        return StyleTransferCustom.network(input_shape=x_train.shape[1:])
 
 
 def initialize_optimizer() -> Union[adam, rmsprop, sgd, adagrad, adadelta, adamax]:
@@ -44,13 +57,56 @@ def initialize_optimizer() -> Union[adam, rmsprop, sgd, adagrad, adadelta, adama
         raise ValueError('An unexpected optimizer name has been encountered.')
 
 
+def create_path(path):
+    directory = os.path.dirname(path)
+    # Create directory if it does not exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def init_callbacks() -> []:
+    callbacks = []
+    if not omit_checkpoint:
+        # Create path for the file.
+        create_path(checkpoint_filepath)
+
+        # Create checkpoint.
+        checkpoint = ModelCheckpoint(checkpoint_filepath, monitor='val_acc', verbose=verbosity,
+                                     save_best_only=True, mode='max')
+        callbacks.append(checkpoint)
+
+    return callbacks
+
+
+def save_results() -> None:
+    # Save weights.
+    if not omit_weights:
+        # Create path for the file.
+        create_path(weights_filepath)
+        # Save weights.
+        model.save_weights(weights_filepath)
+        print('Network\'s weights have been saved as {}.\n'.format(weights_filepath))
+
+    # Save history.
+    if not omit_history:
+        # Create path for the file.
+        create_path(weights_filepath)
+        # Save history.
+        with open(hist_filepath, 'wb') as file:
+            pickle.dump(history.history, file)
+        print('Network\'s history has been saved as {}.\n'.format(hist_filepath))
+
+
 if __name__ == '__main__':
     # Get arguments.
     args = create_training_parser().parse_args()
+    start_point = args.start_point
     omit_weights = args.omit_weights
+    omit_checkpoint = args.omit_checkpoint
     omit_history = args.omit_history
     weights_filepath = args.weights_filepath
     hist_filepath = args.history_filepath
+    checkpoint_filepath = args.checkpoint_filepath
     optimizer_name = args.optimizer
     learning_rate = args.learning_rate
     clip_norm = args.clip_norm
@@ -67,6 +123,7 @@ if __name__ == '__main__':
 
     # Load dataset.
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
     # Preprocess data.
     x_train = preprocess_images(x_train.copy())
     x_test = preprocess_images(x_test.copy())
@@ -74,29 +131,20 @@ if __name__ == '__main__':
     y_test = to_categorical(y_test, 10)
 
     # Create model.
-    model = StyleTransferCustom.network(input_shape=x_train.shape[1:])
+    model = create_model()
 
     # Initialize optimizer.
     optimizer = initialize_optimizer()
 
     # Compile model.
     model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+    # Initialize callbacks list.
+    callbacks_list = init_callbacks()
+
     # Fit network.
-    history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, callbacks=None,
+    history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, callbacks=callbacks_list,
                         validation_data=(x_test, y_test))
 
-    # Save weights.
-    if not omit_weights:
-        model.save_weights(weights_filepath)
-        print('Network\'s weights have been saved as {}.\n'.format(weights_filepath))
-
-    # Save history.
-    if not omit_history:
-        with open(hist_filepath, 'wb') as file:
-            pickle.dump(history.history, file)
-        print('Network\'s history has been saved as {}.\n'.format(hist_filepath))
-
-    # Predict.
-    print('Predicting...\n')
-    scores = model.evaluate(x_test, y_test, batch_size=evaluation_batch_size, verbose=verbosity)
-    print('\nAccuracy: {:.4f} loss: {:.4f}'.format(scores[1] * 100, scores[0]))
+    # Save results.
+    save_results()
