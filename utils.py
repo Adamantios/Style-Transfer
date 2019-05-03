aa1aebaa74603544ae494a28ebbca02828fc6527
Adamantios <manoszaras94@gmail.com>
2019-05-03 21:15:42 +0300

    Remove newlines and correct rho's type.

--- utils.py
+++ utils.py
@@ -123,29 +123,29 @@ def create_training_parser() -> ArgumentParser:
                         choices=OPTIMIZER_CHOICES,
                         help='The optimizer to be used. (default %(default)s).')
     parser.add_argument('-lr', '--learning_rate', type=float, default=LEARNING_RATE, required=False,
-                        help='The learning rate for the optimizer (default %(default)s).\n')
+                        help='The learning rate for the optimizer (default %(default)s).')
     parser.add_argument('-cn', '--clip_norm', type=float, default=CLIP_NORM, required=False,
-                        help='The clip norm for the optimizer (default %(default)s).\n')
+                        help='The clip norm for the optimizer (default %(default)s).')
     parser.add_argument('-cv', '--clip_value', type=float, default=CLIP_VALUE, required=False,
-                        help='The clip value for the optimizer (default %(default)s).\n')
+                        help='The clip value for the optimizer (default %(default)s).')
     parser.add_argument('-b1', '--beta1', type=float, default=BETA1, required=False,
-                        help='The beta 1 for the optimizer (default %(default)s).\n')
+                        help='The beta 1 for the optimizer (default %(default)s).')
     parser.add_argument('-b2', '--beta2', type=float, default=BETA2, required=False,
-                        help='The beta 2 for the optimizer (default %(default)s).\n')
-    parser.add_argument('-rho', type=int, default=float, required=False,
-                        help='The rho for the optimizer (default %(default)s).\n')
+                        help='The beta 2 for the optimizer (default %(default)s).')
+    parser.add_argument('-rho', type=float, default=RHO, required=False,
+                        help='The rho for the optimizer (default %(default)s).')
     parser.add_argument('-m', '--momentum', type=float, default=MOMENTUM, required=False,
-                        help='The momentum for the optimizer (default %(default)s).\n')
+                        help='The momentum for the optimizer (default %(default)s).')
     parser.add_argument('-d', '--decay', type=float, default=DECAY, required=False,
-                        help='The decay for the optimizer (default %(default)s).\n')
+                        help='The decay for the optimizer (default %(default)s).')
     parser.add_argument('-bs', '--batch_size', type=int, default=TRAIN_BATCH_SIZE, required=False,
-                        help='The batch size for the optimization (default %(default)s).\n')
+                        help='The batch size for the optimization (default %(default)s).')
     parser.add_argument('-ebs', '--evaluation_batch_size', type=int, default=EVAL_BATCH_SIZE, required=False,
-                        help='The batch size for the evaluation (default %(default)s).\n')
+                        help='The batch size for the evaluation (default %(default)s).')
     parser.add_argument('-e', '--epochs', type=int, default=EPOCHS, required=False,
-                        help='The number of epochs to train the network (default %(default)s).\n')
+                        help='The number of epochs to train the network (default %(default)s).')
     parser.add_argument('-v', '--verbosity', type=int, default=VERBOSITY, required=False,
-                        help='The verbosity for the optimization procedure (default %(default)s).\n')
+                        help='The verbosity for the optimization procedure (default %(default)s).')
 
     return parser
 
